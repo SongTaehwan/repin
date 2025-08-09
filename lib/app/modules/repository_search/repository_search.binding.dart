ef28c4d2bb1e3d287dbf2da2ad05ac66c5cc7dbc
송태환 <axoghks@gmail.com>
2025-08-09 15:45:59 +0900

    검색 화면 의존성 추가

--- lib/app/modules/repository_search/repository_search.binding.dart
+++ lib/app/modules/repository_search/repository_search.binding.dart
@@ -2,11 +2,36 @@
 import 'package:get/get.dart';
 
 // 🌎 Project imports:
+import 'package:repin/app/core/utils/api/api_client.interface.dart';
+import 'package:repin/app/core/utils/helpers/injection.dart';
+import 'package:repin/app/data/provider/github.provider.dart';
+import 'package:repin/app/data/provider/github.provider.interface.dart';
+import 'package:repin/app/data/repository/code_repo.interface.dart';
+import 'package:repin/app/data/repository/code_repo.repository.dart';
+import 'package:repin/app/data/services/repository_search.service.dart';
+import 'package:repin/app/data/services/repository_search.service.interface.dart';
 import 'package:repin/app/modules/repository_search/repository_search.controller.dart';
 
 class RepositorySearchBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<RepositorySearchController>(() => RepositorySearchController());
+    // Provider
+    final dio = getIt<IApiClient>().client;
+    Get.lazyPut<GithubProviderInterface>(() => GithubProvider(dio));
+
+    // Repository
+    Get.lazyPut<CodeRepoRepositoryInterface>(
+      () => CodeRepoRepository(Get.find<GithubProviderInterface>()),
+    );
+
+    // Service
+    Get.lazyPut<RepositoryServiceInterface>(
+      () => RepositorySearchService(Get.find<CodeRepoRepositoryInterface>()),
+    );
+
+    // Controller
+    Get.lazyPut<RepositorySearchController>(
+      () => RepositorySearchController(Get.find<RepositoryServiceInterface>()),
+    );
   }
 }
