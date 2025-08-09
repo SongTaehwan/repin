8defe8b8cc4dbcb656e75f8b16b2fad9c035cb05
송태환 <axoghks@gmail.com>
2025-08-09 16:44:17 +0900

    도메인 모델 추가

--- lib/app/data/model/repository.model.dart
+++ lib/app/data/model/repository.model.dart
@@ -1,1 +1,54 @@
+// 📦 Package imports:
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+// 🌎 Project imports:
+import 'package:repin/app/data/provider/dto/repository.dto.dart' as dto;
+
+part 'repository.model.freezed.dart';
+part 'repository.model.g.dart';
+
+@freezed
+abstract class Repository with _$Repository {
+  const factory Repository({
+    required int id,
+    required String name,
+    required String ownerName,
+    required int ownerId,
+    required String? ownerProfileUrl,
+    required String repositoryUrl,
+    required DateTime createdAt,
+    required DateTime updatedAt,
+    required DateTime pushedAt,
+    required int forksCount,
+    required int openIssuesCount,
+    required int stargazersCount,
+    required int watchersCount,
+    required String? license,
+    required String? language,
+    required String description,
+  }) = _Repository;
+
+  factory Repository.fromJson(Map<String, dynamic> json) =>
+      _$RepositoryFromJson(json);
+
+  factory Repository.fromDto(dto.Repository dto) {
+    return Repository(
+      id: dto.id,
+      name: dto.name,
+      ownerName: dto.owner?.login ?? "deleted user",
+      ownerId: dto.owner?.id ?? -1,
+      ownerProfileUrl: dto.owner?.avatarUrl,
+      repositoryUrl: dto.htmlUrl,
+      createdAt: dto.createdAt,
+      updatedAt: dto.updatedAt,
+      pushedAt: dto.pushedAt,
+      forksCount: dto.forksCount,
+      openIssuesCount: dto.openIssuesCount,
+      stargazersCount: dto.stargazersCount,
+      watchersCount: dto.watchersCount,
+      license: dto.license?.name,
+      language: dto.language,
+      description: dto.description ?? "",
+    );
+  }
+}
