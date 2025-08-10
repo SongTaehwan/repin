2e83529e89dd79442d15f523e5ffede5b13adfc4
송태환 <axoghks@gmail.com>
2025-08-10 22:25:53 +0900

    탭 컨트롤러 라우트 이동으로 제거 후 다시 화면으로 돌아올 때 자동 복원

--- lib/app/modules/app_container/app_container.binding.dart
+++ lib/app/modules/app_container/app_container.binding.dart
@@ -7,6 +7,10 @@ import 'package:repin/app/modules/app_container/app_container.controller.dart';
 class AppContainerBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<AppContainerController>(() => AppContainerController());
+    Get.lazyPut<AppContainerController>(
+      () => AppContainerController(),
+      // 라우트 이동으로 제거 후 다시 화면으로 돌아올 때 자동 복원
+      fenix: true,
+    );
   }
 }
